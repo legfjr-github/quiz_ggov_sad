71b86cdc70522bc12961b3ff27e2be9273f1b52c
legfjr-github <146836616+legfjr-github@users.noreply.github.com>
2026-06-09 06:46:40 -0300

    Add files via upload

---
+++ questoes_hard.js
@@ -0,0 +1,13 @@
+// Módulo 2
+const qsMod2Aulas1_2 = [];
+const qsMod2Aulas3_4 = [];
+
+// Módulo 3
+const qsMod3Aula1 = [];
+const qsMod3Aula2 = [];
+const qsMod3Aulas3_4 = [];
+
+// Módulo 4
+const qsMod4Aula1 = [];
+const qsMod4Aula2 = [];
+const qsMod4Aulas3_4 = [];
